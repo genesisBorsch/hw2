f5ee838334110b1bc1d61630c5d51bf6b657164c
Aaron Kolker <akolker3@gmail.com>
2016-02-11 09:28:44 -0500

    Added ArrayList Example

---
+++ ArraysExample.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+class ArraysExample {
+	public static void main(String[] args) {
+		///////
+		//Array
+		///////
+		String[] foods = new String[10];
+		Arrays.fill(foods,"popcorn");
+		System.out.print(foods);
+		for (int i = 0; i<foods.length;i++) {
+			System.out.println(foods[i]);
+		}
+		
+		///////////////////
+		//ArrayList Example
+		///////////////////
+		ArrayList<String> moreFood;
+		moreFood = new ArrayList<String>();
+		moreFood.add("candy");
+		moreFood.add("kale");
+		moreFood.add("pumpkin");
+		System.out.println(moreFood);
+	}
+}
